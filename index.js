5ecd8a1a7ee24cd42df3d5425438e3e423b62835
Nicolas Mantzos <georgeos.nicolas83@gmail.com>
2021-10-13 17:52:05 -0300

    [12] enchancements

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ app.use(cors());
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
-environment.configUser();
-environment.configProject();
 environment.configEnv();
 
 require('./src/routes/router')(app);
