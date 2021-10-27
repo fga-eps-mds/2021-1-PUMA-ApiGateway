8ecb55dcd2743aa7eddcb9ae8e1be526c073fbcf
jpmartins201 <jpmartins201@live.com>
2021-10-27 00:20:26 -0300

    [#12] Changing URL_HOM to dev environment

--- src/config/environment.js
+++ src/config/environment.js
@@ -5,6 +5,7 @@ module.exports = {
     if (process.env.ENVIRONMENT === 'dev') {
       global.URL_USER = `http://${process.env.IP_ADDRESS}:3001`;
       global.URL_PROJECT = `http://${process.env.IP_ADDRESS}:3000`;
+      global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'test') {
       global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
