e25d6e203cd7cbe2a087262345afb242dc18ff6c
jpmartins201 <jpmartins201@live.com>
2021-11-06 19:47:36 -0300

    [#86] Updating GET subject route

--- src/routes/subjectRouter.js
+++ src/routes/subjectRouter.js
@@ -8,7 +8,7 @@ require('../config/environment');
 const routes = express.Router()
 routes.get('/', async (req, res) => {
     subjectController.getSubjects(req.body).then((response) => {
-        res.status(200).json({ response });
+        res.status(200).json(response.data);
     }).catch((response) => {
         res.status(400).json({ response });
     });
@@ -24,7 +24,7 @@ routes.post('/', async (req, res) => {
 
 routes.put('/', async (req, res) => {
     subjectController.updateSubject(req.body).then((response) => {
-        res.status(200).json({ response });
+        res.status(200).json(response.data);
     }).catch((response) => {
         res.status(400).json({ response });
     });
@@ -32,7 +32,8 @@ routes.put('/', async (req, res) => {
 
 routes.get('/getSubject/:subjectId', async (req, res) => {
     subjectController.getSubject(req.params.subjectId).then((response) => {
-        res.status(200).json({ response });
+        console.log(response.data);
+        res.status(200).json(response.data);
     }).catch((response) => {
         res.status(400).json({ response });
     });
@@ -49,69 +50,3 @@ routes.delete('/delete/:subjectId', async (req, res) => {
 });
 
 module.exports = routes;
-
-// module.exports = {
-//     getSubjects: (body) => {
-//         const Url = `${global.URL_USER}/disciplina`;
-//         const reqBody = body;
-//         return new Promise((resolve, reject) => {
-//             axios.get(Url, reqBody).then((response) => {
-//                 resolve(response);
-//             }).catch((error) => {
-//                 reject(error);
-//             });
-//         });
-//     },
-//     addSubject: (body) =>  {
-//         const Url = `${global.URL_USER}/disciplina`;
-//         const reqBody = body;
-//         return new Promise((resolve, reject) => {
-//             axios.post(Url, reqBody).then((response) => {
-//                 resolve(response);
-//             }).catch((error) => {
-//                 reject(error);
-//             });
-//         });
-//     },
-//     getSubject: (subjectIdParam) => {
-//         const url = `${global.URL_USER}/disciplina/` + subjectIdParam;
-//         return new Promise((resolve, reject) => {
-//             axios.get(url).then((response) => {
-//                 resolve(response);
-//             }).catch((error) => {
-//                 reject(error);
-//             });
-//         });
-//     },
-//     updateSubject: (subject) => {
-//         const url = `${global.URL_USER}/disciplina/` + subject.subjectid;
-//         return new Promise((resolve, reject) => {
-//             axios.put(url, subject).then((response) => {
-//                 resolve(response);
-//             }).catch((error) => {
-//                 reject(error);
-//             });
-//         });
-//     },
-//     deleteSubject: (subjectId) => {
-//         const url = `${global.URL_USER}/disciplina/` + subjectId;
-//         return new Promise((resolve, reject) => {
-//             axios.delete(url).then((response) => {
-//                 resolve(response);
-//             }).catch((error) => {
-//                 reject(error);
-//             });
-//         });
-//     },
-//     updateSubject: (body) =>  {
-//         const url = `${global.URL_USER}/disciplina`;
-//         const reqBody = body;
-//         return new Promise((resolve, reject) => {
-//             axios.put(url, reqBody).then((response) => {
-//                 resolve(response);
-//             }).catch((error) => {
-//                 reject(error);
-//             });
-//         });
-//     },
-// };
